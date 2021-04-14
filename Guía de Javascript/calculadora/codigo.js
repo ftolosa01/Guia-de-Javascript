e551d5eaa5224830f22c0b671ea4b9f2f69f9731
ftolosa01 <f.tolosa01@ufromail.cl>
2021-04-14 19:08:44 -0400

    Add files via upload

--- Guía de Javascript/calculadora/codigo.js
+++ Guía de Javascript/calculadora/codigo.js
@@ -6,32 +6,39 @@ var resultado = document.getElementById("resultado")
 var inputUno = document.getElementById("input-uno");
 var inputDos = document.getElementById("input-dos");
 
+function validar(){
+    if ((inputUno.value.length == 0) || (inputDos.value.length ==0)) {
+        alert('falta Informacion');
+        return false
+      }  
+}
+
 btn.addEventListener("click",function(){
 
 var n1 = inputUno.value;
 var n2 = inputDos.value;
-
+validar();
 resultado.innerHTML = suma(n1,n2);
 });
 btnrestar.addEventListener("click",function(){
 
     var n1 = inputUno.value;
     var n2 = inputDos.value;
-
+    validar();
     resultado.innerHTML = restar(n1,n2);
 });
 btnmultiplicar.addEventListener("click",function(){
 
         var n1 = inputUno.value;
         var n2 = inputDos.value;
-
+        validar();
         resultado.innerHTML = multiplicar(n1,n2);
 });
 btndividir.addEventListener("click",function(){
 
     var n1 = inputUno.value;
     var n2 = inputDos.value;
-
+    validar();
     resultado.innerHTML = dividir(n1,n2);
 });   
 function suma(n1, n2){
@@ -51,5 +58,8 @@ function dividir(n1, n2){
         }else
         return parseInt(n1) / parseInt(n2);
 }
+
+
+
 
     
